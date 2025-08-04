7b4a5c07ff408a47e7e480baa2752b53778128cd
FanQiBig <3443282125@qq.com>
2025-08-04 12:00:40 +0800

    update notes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.vuejs.org/config/app-configs
-export default {
+export default defineConfig({
   title: "我的学习笔记",
   description: "记录我的学习过程",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
     nav: [
       { text: "首页", link: "/" },
       { text: "算法笔记", link: "/algorithm" }
     ]
   }
-}
+})
